1fcb428c6baec30515e0eaa9d220bf50f55767b5
Ahiruk <santoyal@oop-santoyal.dguo2cjoo0aennawf4210q2g4h.phxx.internal.cloudapp.net>
2022-11-15 14:40:21 +0000

    First Commit

--- Movie Tickets/Sala.py
+++ Movie Tickets/Sala.py
@@ -2,6 +2,19 @@ class Sala:
     def __init__(self) -> None:
         self.disponibilidad= 32
         self.numero_asiento =None
+    @property
+    def disponibilidad(self):
+        return self.disponibilidad
+    @property
+    def numero_asiento(self):
+        return self.numero_asiento
+
     def reserva(self, disponibilidad : int):
         disponibilidad=disponibilidad-1
+        return disponibilidad
+    def cancelar(self, disponibilidad : int):
+        disponibilidad=disponibilidad+1
+        return disponibilidad
+
+
 
